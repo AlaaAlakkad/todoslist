5647ee6cd8b346fff11168fcfd76a01b5d8be6dd
alaa ak <alaaalakkad@gmail.com>
2018-07-22 20:48:17 +0200

    delete added

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -2,12 +2,12 @@
 @section('content')
     <a href="/todo/{{$todo->id}}" class="btn">Go Back</a>
     <h1>Edit</h1>
-    {!! Form::open(['action' => ['TodosController@update', $todo->id], 'method'=>'POST'])!!}
+    {!! Form::open(['action' => ['TodosController@update', $todo->id], 'method'=>'POST']) !!}
         {{ Form::appTextForm('text', $todo->text) }}
         {{ Form::appTextarea('body', $todo->body) }}
         {{ Form::appTextForm('due', $todo->due) }}
         {{ Form::hidden('_method', 'PUT')}}
         {{ Form::appSubmit('Submit', ['class'=>'btn btn-primary'])}}
-    {!! Form::close()!!}
+    {!! Form::close() !!}
 
 @endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -3,8 +3,15 @@
 @section('content')
     <a class="btn" href="/">Go Back</a>
     <h1>{{$todo->text}}</h1>
-    <span class="badge badge-danger mb-3">{{$todo->due}}</span>
+    <span class="badge badge-info mb-3">{{$todo->due}}</span>
     <p class="mb-3">{{$todo->body}}</p>
 
-    <a class="btn" href="/todo/{{$todo->id}}/edit">Edit</a>
+    <div class="form-inline">
+        <a class="btn" href="/todo/{{$todo->id}}/edit">Edit</a>
+        {!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'method'=>'POST']) !!}
+        {{ Form::hidden('_method', 'DELETE')}}
+        {{ Form::appSubmit('Delete', ['class'=>'btn btn-danger btn-sm'])}}
+        {!! Form::close() !!}
+    </div>
+
 @endsection
