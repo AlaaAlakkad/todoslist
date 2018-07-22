8d2aaad808ab30ddb03d30c44ef48f2a069f3969
alaa ak <alaaalakkad@gmail.com>
2018-07-22 14:50:15 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+    <div class="container mt-3">
+        @yield('content')
+    </div>
+
+    <footer id="footer" class="text-center">
+        <p>Copyright &copy; 2018 Todolist</p>
+    </footer>
+</body>
+</html>
